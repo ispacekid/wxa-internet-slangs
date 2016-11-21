54c2496afcce48692022df64854f63fba416d9ba
spacekid <dksnow2002@gmail.com>
2016-11-21 17:19:42 +0800

    added tabbar and new pages

---
+++ app/pages/search/search.js
@@ -0,0 +1,30 @@
+var app = getApp()
+
+Page({
+  data: {
+  },
+  // 监听页面加载
+  onLoad () {
+    console.log('Search Page: onLoad.')
+  },
+  // 监听页面初次渲染完成
+  onReady () {
+    console.log('Search Page: onReady.')
+  },
+  // 监听页面显示
+  onShow () {
+    console.log('Search Page: onShow.')
+  },
+  // 监听页面隐藏
+  onHide () {
+    console.log('Search Page: onHide.')
+  },
+  // 监听页面卸载
+  onUnload () {
+    console.log('Search Page: onUnload.')
+  },
+  // 监听用户下拉刷新动作
+  onPullDownRefresh () {
+    console.info('Search Page: onPullDownRefresh.')
+  }
+})
