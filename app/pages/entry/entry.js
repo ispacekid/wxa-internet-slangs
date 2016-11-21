d246f292168a39b8cffdcd43e8dc7558458d53db
spacekid <dksnow2002@gmail.com>
2016-11-21 15:02:00 +0800

    added lifecycle functions

--- app/pages/entry/entry.js
+++ app/pages/entry/entry.js
@@ -7,7 +7,9 @@ Page({
     attachments: {},
     footnotes: {}
   },
+  // 监听页面加载
   onLoad: function(options) {
+    console.log('Entry Page: onLoad.'),
     // 设定数据
     this.setData({
       item: app.globalData.api.data[Number(options.index)],
@@ -18,5 +20,25 @@ Page({
     wx.setNavigationBarTitle({
       title: app.globalData.api.data[Number(options.index)].title
     })
-  }
+  },
+  // 监听页面初次渲染完成
+  onReady () {
+    console.log('Entry Page: onReady.')
+  },
+  // 监听页面显示
+  onShow () {
+    console.log('Entry Page: onShow.')
+  },
+  // 监听页面隐藏
+  onHide () {
+    console.log('Entry Page: onHide.')
+  },
+  // 监听页面卸载
+  onUnload () {
+    console.log('Entry Page: onUnload.')
+  },
+  // 监听用户下拉刷新动作
+  onPullDownRefresh () {
+    console.log('Entry Page: onPullDownRefresh.')
+  },
 })
